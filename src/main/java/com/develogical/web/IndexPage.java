a7f2fef4efb6ae83836cae4304fa520b9589455f
julekwiatrak <julekwiatrak@gmail.com>
2019-04-11 13:45:24 +0100

    Test change to index file - font size and colour

--- src/main/java/com/develogical/web/IndexPage.java
+++ src/main/java/com/develogical/web/IndexPage.java
@@ -7,7 +7,7 @@ public class IndexPage extends HtmlPage {
     @Override
     protected void writeContentTo(PrintWriter writer) {
         writer.println(
-                "<h1>Welcome! :)</h1>" +
+                "<h1 style=\"color:red;font-size:40px;\">Welcome! :)</h1>" +
                 "<p>Enter your query in the box below: " +
                   "<form><input type=\"text\" name=\"q\" />" +
                     "<input type=\"submit\">" +
